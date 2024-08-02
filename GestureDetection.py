95ae3aff57c9d610473d4f13097818c25267b963
Yodaman07 <ayaan.irshad@gmail.com>
2024-08-01 22:36:28 -0700

    Gestures are detected on screen

--- GestureDetection.py
+++ GestureDetection.py
@@ -15,6 +15,9 @@ class GestureDetector:
         self.recognizer: vision.GestureRecognizer = None
         self.maxGestureCount = gestureSpace
         self.gestures: [] = []
+        # https: // ai.google.dev / edge / mediapipe / solutions / vision / gesture_recognizer
+        self.possibleGestures = ["Unknown", "Closed_Fist", "Open_Palm", "Pointing_Up", "Thumb_Down", "Thumb_Up",
+                                 "Victory", "ILoveYou"]
 
         self.frame_count: int = 0
         self.cam: cv.VideoCapture = None
@@ -29,21 +32,27 @@ class GestureDetector:
             data = result.gestures[0][0]
             name = data.category_name
             confidence = data.score
-            if name != "None":
-                gestureData = {"Name": name, "Confidence": round(confidence * 100, 2), "Timestamp": timestamp_ms}
-                if not self.gestures:
-                    self.gestures.append(gestureData)
-                    return
 
-                if len(self.gestures) < 10:
-                    self.gestures.append(gestureData)
-                else:
-                    self.gestures[0:9] = self.gestures[1:10]  # shifts list values to make space for the newest one
-                    self.gestures[-1] = gestureData
+            gestureData = {"Name": name, "Confidence": round(confidence * 100, 2), "Timestamp": timestamp_ms}
+            print(gestureData)
+            self.appendGestureData(gestureData)
 
         else:
-            pass
-            # print(f"No pose detected, timestamp: {timestamp_ms}")
+            # TODO Add diff between None and Unknown
+            gestureData = {"Name": None, "Confidence": None, "Timestamp": timestamp_ms}
+            print(gestureData)
+            self.appendGestureData(gestureData)
+
+    def appendGestureData(self, gestureData):
+        if not self.gestures:
+            self.gestures.append(gestureData)
+            return
+
+        if len(self.gestures) < 10:
+            self.gestures.append(gestureData)
+        else:
+            self.gestures[0:9] = self.gestures[1:10]  # shifts list values to make space for the newest one
+            self.gestures[-1] = gestureData
 
     def configRecognizer(self, model_path):
         base_options = python.BaseOptions(model_asset_path=model_path)
@@ -93,10 +102,12 @@ class GestureDetector:
 
         self.frame_count = 0
 
-    def getCurrentFrame(
-            self) -> np.array:  # camera information to get called every frame <-- must be put in a while loop
+    def getCurrentFrame(self) -> np.array:  # camera information to get called every frame <-- must be put in a while loop
         self.frame_count += 1
         retrieved, frame = self.cam.read()
+
+        mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        self.recognizer.recognize_async(mp_img, self.frame_count) # TODO Fix the timestamp ms thing instead of just returning the frame count
 
         frame = cv.resize(frame, (int(self.width), int(self.height)))
         if not retrieved:
@@ -104,7 +115,4 @@ class GestureDetector:
             return
         return frame
 
-# g = GestureDetector(10)
-# g.run()
-# print(g.gestures)
 # TODO: Fix issue where you can only get the gestures after you finish the program. Run simultaneously or smth
--- GestureScreen.py
+++ GestureScreen.py
@@ -13,13 +13,31 @@ class GestureScreen:  # sets up a pygame screen connected to a live stream, dete
     def runLoop(self):
         gd = GestureDetector(10)
         gd.initStream()
-
         self.running = True
 
+        gap = 10
+        width = (((gd.height / 4) + gap) * len(gd.possibleGestures)) - gap  # 7 gap sizes and 8 icon sizes
+
         streamArea = pygame.Surface((gd.width, gd.height))
-        streamArea.fill("green")  # will only paint once
+        gestureIconLayout = pygame.Surface((width, gd.height / 4))
+        gestureIconLayout.fill("green")
+
+        iconOffset = gap + (gd.height / 4)
+
 
         while self.running:
+            addedOffset = 0
+            for gesture in gd.possibleGestures:
+                color = "purple"
+                if gd.gestures != []:
+                    if gd.gestures[-1]["Name"] == gesture:
+                        color = "red" # red means detected
+
+                pygame.draw.rect(gestureIconLayout, color, pygame.Rect(addedOffset, 0, gd.height / 4, gd.height / 4))
+
+                addedOffset += iconOffset
+
+
             currentFrame = gd.getCurrentFrame()
 
             # code from https://stackoverflow.com/questions/19306211/opencv-cv2-image-to-pygame-image
@@ -27,9 +45,10 @@ class GestureScreen:  # sets up a pygame screen connected to a live stream, dete
                                           "BGR")
 
             self.screen.fill("white")
+            self.screen.blit(gestureIconLayout, (0, gd.height + 10))
             self.screen.blit(streamArea, (0, 0))
 
-            streamArea.blit(img, (0,0))
+            streamArea.blit(img, (0, 0))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
